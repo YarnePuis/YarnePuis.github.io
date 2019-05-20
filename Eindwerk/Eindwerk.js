c85a4d15152d31731f6ca5bae89a320f69e5ed2f
YarnePuis <43605518+YarnePuis@users.noreply.github.com>
2019-05-20 15:41:42 +0200

    Verder gewerkt aan pc deel

--- Eindwerk/Eindwerk.js
+++ Eindwerk/Eindwerk.js
@@ -4,23 +4,36 @@
 
     window.addEventListener('load', function () {
 
-
+        RandomGenerator();
+        
         // //gives a name to the image
         let photo = document.getElementById('PauloCallebaut');
 
         let spaceW = screen.height - photo.height;
         let spaceH = screen.width - photo.width;
 
-        document.getElementById('PressStart').addEventListener('click', function () {
-            document.getElementById('Start').classList.add('Start--Hide');
-            Easy();
-        });
-
-        function Easy() {
-            // //set the interval to 750ms
-            setInterval(MoveIt, 750);
+        function RandomGenerator () {
+            document.getElementById('PressStart').addEventListener('click', function () {
+                document.getElementById('Start').classList.add('Start--Hide');
+    
+                if (window.location.href == 'http://127.0.0.1:5500/Eindwerk/Clvl1.html')
+                {
+                    //The interval will set to 1.5s
+                    setInterval(MoveIt, 1500);
+                }
+                else if (window.location.href == 'http://127.0.0.1:5500/Eindwerk/Clvl2.html')
+                {
+                    //The interval will set to 900ms
+                    setInterval(MoveIt, 900);
+                }
+                else 
+                {
+                    //The interval will set tot 500ms
+                    setInterval(MoveIt, 500);
+                }
+            });
         };
-
+        
         function MoveIt() {
 
             //random movement for the image
@@ -31,8 +44,10 @@
         let teller = 0;
         document.getElementById('PauloCallebaut').addEventListener('click', function () {
 
-            if (teller == 4) {
+            if (teller == 4) { //When Teller is 5, You can go to the next level.
                 document.getElementById('LevelCom').classList.remove('LevelCom--Hide');
+                teller++;
+                document.getElementById('Teller').innerHTML = teller;
             } else {
                 teller++;
                 console.log(teller);
