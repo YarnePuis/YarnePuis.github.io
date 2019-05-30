ab7b2402de5aadc2b8a353220002a45aecdaa756
YarnePuis <43605518+YarnePuis@users.noreply.github.com>
2019-05-30 12:13:54 +0200

    details

--- Eindwerk/Eindwerk1/Eindwerk.js
+++ Eindwerk/Eindwerk1/Eindwerk.js
@@ -3,54 +3,59 @@
     'use strict';
 
     /*The global score */
-    let score = 20;
+    let score = 50;
 
     //A teller to count
-     let teller = 0;
+    let teller = 0;
 
     //gives a name to the image
     let photo = document.getElementById('PauloCallebaut');
-    
+
     let auwtch = new Audio('../Eindwerk1/audio/PauloAuwtch1.mp4');
     auwtch.volume = 1.0;
-    
+
     let boom = new Audio('../Eindwerk1/audio/Boom.mp3');
     boom.volume = 1.0;
 
     let level = 0;
 
-    function Level2(){
+    let enabled = true;
+
+    //Here are the codes for the different levels
+    function Level2() {
         document.body.style.backgroundImage = "Url('../Eindwerk1/fotos/BackgroundLvl2.jpg')";
         document.getElementById('NextLevels').innerHTML = 'Level 2';
 
-        
+
     }
 
     function Level3() {
         document.body.style.backgroundImage = "Url('../Eindwerk1/fotos/BackgroundLvl3.jpg')";
         document.getElementById('NextLevels').innerHTML = 'Level 3';
     }
-    
+
+    //this is the code for the last level
     function EndlessLevel() {
 
-        
+
         document.body.style.backgroundImage = "Url('../Eindwerk1/fotos/BackgroundEndless.jpg')";
         document.getElementById('NextLevels').innerHTML = 'Endless Level';
-        document.getElementById('ScoreBoard').style.color = 'white'
-
+        document.getElementById('ScoreBoard').style.color = 'white';
+        document.getElementById('PauloCallebaut').classList.add('hiddem');
         document.getElementById('LevelBoard').classList.add('hidden');
         document.getElementById('EndlessBoard').classList.remove('hidden');
 
         document.getElementById('EndlessLvl').addEventListener('click', function () {
             document.getElementById('EndlessBoard').classList.add('hidden');
+            document.getElementById('PauloCallebaut').classList.remove('hidden');
             setInterval(function () {
                 MoveIt(0)
-            }, 13000);
+            }, 1300);
 
-        document.getElementById('TellerFrame').classList.add('hidden');
-        document.getElementById('EndlessScore').classList.remove('hidden');
-        teller = 6;
-        console.log(teller);
+            document.getElementById('TellerFrame').classList.add('hidden');
+            document.getElementById('EndlessScore').classList.remove('hidden');
+            teller = 6;
+            enabled = false;
 
         });
 
@@ -86,12 +91,12 @@
         if (score <= 0) {
             document.getElementById('GameOver').classList.remove('hidden');
             clearInterval(scoreInterval);
-            
+
             document.getElementById('PauloCallebaut').classList.add('hidden');
             document.getElementById('yourScore').innerHTML = teller;
-            
+
         }
-        
+
     }
 
     function Teller() {
@@ -114,11 +119,16 @@
                 //Play the boom sound
                 boom.play();
 
+                //you will lose 100pts when click on the wrong image
                 score -= 100;
                 document.getElementById('ScoreBoard').innerHTML = score;
             } else {
 
-                //score += 20;
+                //recieve 10pts when click on the right image
+                if (enabled === true) {
+                    score += 10;
+                }
+
                 document.getElementById('ScoreBoard').innerHTML = score;
 
                 //When pressed on the picture, a sound will appear.
@@ -127,7 +137,8 @@
                 teller++;
                 document.getElementById('Teller').innerHTML = teller;
 
-                let dir = document.getElementById('EndlessScore'),endGameScore;
+                let dir = document.getElementById('EndlessScore'),
+                    endGameScore;
 
                 localStorage.setItem('endGameScore', teller);
                 endGameScore = localStorage.getItem('endGameScore');
@@ -140,52 +151,49 @@
 
     let interval = null;
 
-    function intervals () {
+    function intervals() {
 
-    
+        //An interval for the different levels
         switch (level) {
-            case 1: 
+            case 1:
                 interval = setInterval(function () {
                     MoveIt(0.2)
-                }, 13000);
-
-                console.log(level);
+                }, 1300);
                 break;
-            case 2: 
-            clearInterval(interval);
+            case 2:
+                clearInterval(interval);
                 Level2();
 
                 interval = setInterval(function () {
-                    MoveIt(0.35)}, 10000
-                );
+                    MoveIt(0.35)
+                }, 1100);
                 break;
             case 3:
                 clearInterval(interval);
                 Level3();
 
                 interval = setInterval(function () {
-                    MoveIt(0.2)}, 13000
-                );
+                    MoveIt(0.5)
+                }, 1000);
                 break;
             case 4:
                 clearInterval(interval);
 
-                console.log(level);
                 EndlessLevel();
         }
     }
 
-    function furtherLevel () {
+    function furtherLevel() {
 
+        //To start a new level
         document.getElementById('LevelBoard').addEventListener('click', function () {
 
             document.getElementById('LevelBoard').classList.add('hidden');
             document.getElementById('Start').classList.remove('hidden');
-        })};
+        })
+    };
 
     window.addEventListener('load', function () {
-
-
 
         Teller();
         /*every second, your score will drop*/
@@ -197,8 +205,8 @@
             console.log(level);
 
             document.getElementById('Start').classList.add('hidden');
-            document.getElementById('PauloCallebaut').classList.remove('hidden');   
-             
+            document.getElementById('PauloCallebaut').classList.remove('hidden');
+
             intervals();
             furtherLevel();
 
